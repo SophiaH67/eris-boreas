696414330b8aa66563449de40735a3aff70ed0c7
marnixah <marnix@hagehoeve.com>
2022-04-26 00:45:18 +0200

    Revert "test(commands): :test_tube: Command Exception test"
    This reverts commit 1ef13b17255badd0d8a82e5e0f178523c7d7b2db.

--- test/CommandException.spec.ts
+++
@@ -1,8 +0,0 @@
-import CommandException from '../src/classes/commands/CommandException';
-
-describe('CommandException', () => {
-  it('should work', () => {
-    const commandException = new CommandException();
-    expect(commandException.message).toBe('Command not found');
-  });
-});
