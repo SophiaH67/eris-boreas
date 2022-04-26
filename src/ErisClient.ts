c88ce5d653153bbcf67b3b697882c2ea3ef558c5
marnixah <marnix@hagehoeve.com>
2022-04-26 21:33:53 +0200

    feat(base): :sparkles: eris.loadCommands

--- src/ErisClient.ts
+++ src/ErisClient.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Client, Message } from 'discord.js';
+import { readdirSync } from 'fs';
 import { createClient } from 'redis';
 import Command from './conversation/Command';
 import ConversationManager from './conversation/ConversationManager';
@@ -37,5 +40,15 @@ export default class ErisClient {
     if (!conversation.isWaitingForReply()) {
       await conversation.executeDirectives();
     }
+  }
+
+  loadCommands(path: string) {
+    const files = readdirSync(path);
+    files.forEach(file => {
+      if (file.endsWith('.js') || file.endsWith('.ts')) {
+        const command = require(`${path}/${file}`);
+        this.commands.push(new command.default(this) as Command);
+      }
+    });
   }
 }
